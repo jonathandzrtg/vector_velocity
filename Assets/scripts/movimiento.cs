f72f0fbd0efafdb6e3dc11844f4f89fa415c54c4
jonathandzrtg <jhntndz.jd@gmail.com>
2024-05-13 16:13:07 -0500

    personajes

--- Assets/scripts/movimiento.cs
+++ Assets/scripts/movimiento.cs
@@ -6,8 +6,8 @@ public class movimiento : MonoBehaviour
 {
   
     public float velocidadRotacion = 50.0f;
+    public float speed = 5f; // Velocidad de movimiento del obstáculo
 
-   
     void Start()
     {
         
@@ -16,8 +16,8 @@ public class movimiento : MonoBehaviour
 
     void Update()
     {
-        
         transform.Rotate(Vector3.up, velocidadRotacion * Time.deltaTime);
+        //transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 }
 
