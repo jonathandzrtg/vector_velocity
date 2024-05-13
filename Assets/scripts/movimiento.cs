23e2e46c1181db909400fc5674883068abf68f9a
Jonathan Diaz <106851304+jonathandzrtg@users.noreply.github.com>
2024-05-13 16:35:01 -0500

    Update movimiento.cs

--- Assets/scripts/movimiento.cs
+++ Assets/scripts/movimiento.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class movimiento : MonoBehaviour
 {
-  
+ 
     public float velocidadRotacion = 50.0f;
-    public float speed = 5f; // Velocidad de movimiento del obstáculo
-
+    public float speed = 5f; // Velocidad de movimiento del obstï¿½culo
+ 
     void Start()
     {
-        
+       
     }
-
-
+ 
+ 
     void Update()
     {
         transform.Rotate(Vector3.up, velocidadRotacion * Time.deltaTime);
         //transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
+     void OnCollisionEnter2D(Collision2D collision)  
+       {         // Verifica si la colisiÃ³n es con el personaje
+       if (collision.gameObject.CompareTag("Jugador"))        
+        {             // Destruye este objeto            
+        Destroy(gameObject);      
+           }     }
 }
-
