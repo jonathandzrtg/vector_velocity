7fa60c2ceb1b13464449d79d9c9846c06d578180
Jonathan Diaz <106851304+jonathandzrtg@users.noreply.github.com>
2024-05-13 16:37:12 -0500

    Update movimiento.cs

--- Assets/scripts/movimiento.cs
+++ Assets/scripts/movimiento.cs
@@ -1,12 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class movimiento : MonoBehaviour
 {
-    public float velocidadRotacion = 50.0f; // Velocidad a la que rota la moneda
-
+ 
+    public float velocidadRotacion = 50.0f;
+    public float speed = 5f; // Velocidad de movimiento del obst�culo
+ 
+    void Start()
+    {
+       
+    }
+ 
+ 
     void Update()
     {
-        // Hace que la moneda rote continuamente alrededor de su eje vertical
         transform.Rotate(Vector3.up, velocidadRotacion * Time.deltaTime);
+        //transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
+     void OnCollisionEnter2D(Collision2D collision)  
+       {         // Verifica si la colisión es con el personaje
+       if (collision.gameObject.CompareTag("Jugador"))        
+        {             // Destruye este objeto            
+        Destroy(gameObject);      
+           }     }
 }
